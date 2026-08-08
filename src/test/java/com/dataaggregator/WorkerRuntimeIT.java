d9a9de40c7a65439146c9eb5b650035c6510180d
qingru97 <qingru97@gmail.com>
2026-08-08 17:47:42 +0800

    test: stop worker listeners before container shutdown

--- src/test/java/com/dataaggregator/WorkerRuntimeIT.java
+++ src/test/java/com/dataaggregator/WorkerRuntimeIT.java
@@ -4,8 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.dataaggregator.config.DataAggregatorProperties;
 import com.dataaggregator.support.IntegrationTestContainers;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -22,10 +25,18 @@ class WorkerRuntimeIT extends IntegrationTestContainers {
     @Autowired
     private DataAggregatorProperties properties;
 
+    @Autowired
+    private RabbitListenerEndpointRegistry listenerRegistry;
+
     @Test
     void workerRuntimeStartsAndDeclaresPlaceholderQueue() {
         assertThat(properties.runtimeMode()).isEqualTo("worker");
         assertThat(amqpAdmin.getQueueInfo(properties.worker().placeholderQueue()))
                 .isNotNull();
+    }
+
+    @AfterEach
+    void stopListenersBeforeContainersShutdown() {
+        listenerRegistry.getListenerContainers().forEach(MessageListenerContainer::stop);
     }
 }
